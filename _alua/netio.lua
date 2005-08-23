aa3e1f75b34dd41b5cb4b08d5abc38f10a599af3
pedro <pedro@inf.puc-rio.br>
2005-08-23 14:10:37 +0000

    Big restructuring of the protocol code, lots of bug fixes

--- _alua/netio.lua
+++ _alua/netio.lua
@@ -1,30 +1,37 @@
 -- $Id$
-
--- Copyright (c) 2005 Lab//, PUC-Rio
+--
+-- Copyright (c) 2005 Pedro Martelletto <pedro@ambientworks.net>
 -- All rights reserved.
+--
+-- This file is part of the Alua Project.
+--
+-- As a consequence, to every excerpt of code hereby obtained, the respective
+-- project's licence applies. Detailed information regarding the licence used
+-- in Alua can be found in the LICENCE file provided with this distribution.
 
--- This file is part of ALua. As a consequence, to every excerpt of code
--- hereby obtained, the respective project's licence applies. Detailed
--- information regarding ALua's licence can be found in the LICENCE file.
+-- This file implements a simple abstraction layer to parse incoming and
+-- prepare outgoing data, according to the protocol used in Alua.
 
--- A simple abstraction layer to parse incoming and prepare outgoing data.
 module("netio")
 
--- Encapsulation of external modules.
 local event = require("event")
 local utils = require("utils")
 
+--
 -- Transform an outgoing '<command>, <argument table>' pair into a string and
 -- send it on the given socket.
+--
 function
-send(sock, cmd, arg)
+netio.send(sock, cmd, arg)
 	sock:send(string.format("%s %s\n", cmd, utils.dump(arg)))
 end
 
+--
 -- Receive and parse incoming data into a '<command>, <argument table>' pair.
+--
 function
-recv(sock)
-	-- Receive the packet.
+netio.recv(sock)
+	-- Read the packet from the socket.
 	local data, e = sock:receive()
 	if not data then return nil, nil, e end
 
@@ -45,67 +52,109 @@ recv(sock)
 	return cmd, arg
 end
 
--- Generic event handling function. Takes care of the sequence count.
+--
+-- Handle an incoming request.
+--
 function
-handler(sock, context)
+netio.request(sock, context, cmd, data)
+	-- Try to find a handler for it.
+	local handler = context.cmdtab[cmd]
+	if not handler then
+		netio.send(sock, "error", { value = "Invalid command" } )
+		return
+	end
+
+	-- Call the handler.
+	local reply = handler(sock, context, data)
+	if not reply then return end
+
+	-- Keep state.
+	reply.sequence_count = data.sequence_count
+
+	-- Send eventual replies back.
+	netio.send(sock, cmd .. "-reply", reply)
+end
+
+--
+-- Handle an incoming reply.
+--
+function
+netio.reply(context, data)
+	-- Try to find a callback we should call.
+	local callback = context.sequence_table[data.sequence_count]
+
+	-- If there's no callback, then something might be wrong.
+	if not callback then
+--		print("Warning, asynchronous reply with no callback?")
+		return
+	end
+
+	-- We have a match, remove the entry from the sequence table.
+	context.sequence_table[data.sequence_count] = nil
+
+	-- If this was our topmost sequence number, decrement it.
+	if data.sequence_count == context.sequence_count - 1 then
+		context.sequence_count = data.sequence_count - 1
+	end
+
+	-- Hide sequence number from callback.
+	data.sequence_count = nil
+
+	-- Do it, finally.
+	callback(data)
+end
+
+--
+-- Generic event handling function. Takes care of the sequence count, which is
+-- needed since we can share multiple instances of asynchronous requests in a
+-- same channel.
+--
+function
+netio.handler(sock, context)
 	-- Receive and parse the message.
-	local cmd, data, e = recv(sock)
+	local cmd, data, e = netio.recv(sock)
 
-	-- In case of network error, remove the associated event. Specific
-	-- steps regarding the socket's decomissioning can be taken in the
-	-- terminator function called by the event module.
+	-- In case of network error, remove the associated event.
 	if not data then event.del(sock) return end
-	-- In case of parse error, return an error message.
-	if not cmd then send(sock, "error", { value = e }) return end
 
-	-- If the packet is a reply for a request we made, do the callback.
+	-- In case of parse error, return an error message.
+	if not cmd then netio.send(sock, "error", { value = e } ) return end
+
+	-- Check if the packet is a reply for a request we made.
 	if string.find(cmd, "-reply$") then
-		local callback = context.seqtable[data.seqcount]
-		context.seqtable[data.seqcount] = nil
-		data.seqcount = nil
-		if callback then callback(data) end
-		-- If this was our topmost sequence number, decrement it.
-		if data.seqcount == context.seqcount - 1 then
-			context.seqcount = data.seqcount - 1
-		end
+		netio.reply(context, data)
 	else
-		-- Otherwise, the packet holds an incoming request. Try to find
-		-- a handler for it and return the reply, if any.
-		if not context.cmdtab[cmd] then
-			send(sock, "error", { value = "Invalid command" })
-		else
-			local rep = context.cmdtab[cmd](sock, context, data)
-			if rep then
-				-- Send the reply, and keep state.
-				rep.seqcount = data.seqcount
-				send(sock, cmd .. "-reply", rep)
-			end
-		end
+		-- Otherwise, the packet holds an incoming request.
+		netio.request(sock, context, cmd, data)
 	end
 end
 
+--
 -- Issue a protocol command, asynchronously.
+--
 function
-cmd(sock, cmd, arg, callback)
+netio.cmd(sock, cmd, arg, callback)
 	-- Get the socket context.
 	local _, context = event.get(sock)
 
 	-- Arrange the socket's sequence record.
-	context.seqcount = context.seqcount or 0
-	context.seqtable = context.seqtable or {}
+	context.sequence_count = context.sequence_count or 0
+	context.sequence_table = context.sequence_table or {}
 
-	-- Tag the request with the current seqcount, so that we can identify
-	-- the reply when it arrives (as the channel could be shared).
-	arg.seqcount = context.seqcount
-	context.seqtable[arg.seqcount] = callback
-	context.seqcount = context.seqcount + 1
+	-- Tag the request with the current sequence count, so that we can
+	-- identify the reply when it arrives (as the channel could be shared).
+	arg.sequence_count = context.sequence_count
+	context.sequence_table[arg.sequence_count] = callback
+	context.sequence_count = context.sequence_count + 1
 
-	send(sock, cmd, arg)
+	netio.send(sock, cmd, arg)
 end
 
--- Reply to a protocol command.
+--
+-- The spawn algorithm needs to be reviewed, so this kludge can be removed.
+--
 function
-reply(sock, cmd, oldp, newp)
-	newp.seqcount = oldp.seqcount
-	send(sock, cmd .. "-reply", newp)
+netio.spawn_reply(sock, cmd, oldp, newp)
+	newp.sequence_count = oldp.sequence_count
+	netio.send(sock, cmd .. "-reply", newp)
 end
