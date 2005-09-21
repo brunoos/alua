28d4de1b2df44621a2d8d9ea7a88f01a8a93e796
pedro <pedro@inf.puc-rio.br>
2005-09-21 21:33:11 +0000

    new alua.exit() example

--- samples/exit.lua
+++ samples/exit.lua
@@ -1,24 +1,26 @@
+-- public domain
 alua = require("alua")
 
+local app = "myapp"
+
 function
-leave_callback(reply)
-	for proc, termination in reply do
-		if termination.status == "ok" then
-			print("Process " .. proc .. " terminated")
-		end
-	end
+exit_callback(reply)
+	print("reply table is: " .. _alua.utils.dump(reply))
+	alua.exit()
 end
 
 function
 spawn_callback(reply)
- 	for proc, spawn in reply.processes do
-		if spawn.status == "ok" then
-			alua.exit(proc, leave_callback)
-		end
-	end
+	local procs = {}
+ 	for proc in reply.processes do table.insert(procs, proc) end
+	print("sending exist request...")
+	alua.exit(procs, 0, exit_callback)
+end
+
+function start_callback(reply)
+	alua.spawn(app, 4, spawn_callback)
 end
 
 alua.open()
-alua.start("application x")
-alua.spawn("application x", 4, spawn_callback)
+alua.start(app, start_callback)
 alua.loop()
