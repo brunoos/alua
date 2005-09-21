3475ad91b605977cab84fb313aa751fac62b4d88
pedro <pedro@inf.puc-rio.br>
2005-09-21 22:42:17 +0000

    new alua.query() example

--- samples/query.lua
+++ samples/query.lua
@@ -1,16 +1,35 @@
+-- public domain
 alua = require("alua")
 
-function
-query_callback(reply)
-  	if reply.processes then
-		print("Application " .. reply.name .. " exists, and has: ")
-		for _, proc in reply.processes do print(proc) end
-		print("Where " .. reply.master .. " is the master process.")
-	else
-		print("Application " .. reply.name .. " does not exist.")
-	end
+local procs, app = {}, "myapp"
+
+function query3_callback(reply)
+	print("reply table is: " .. _alua.utils.dump(reply))
+	alua.exit(procs); alua.exit()
+end
+
+function query2_callback(reply)
+	print("reply table is: " .. _alua.utils.dump(reply))
+	alua.spawn(app, 5, spawn_callback)
+end
+
+function query1_callback(reply)
+	print("reply table is: " .. _alua.utils.dump(reply))
+	alua.start(app, start_callback)
+end
+
+function spawn_callback(reply)
+	for proc in reply.processes do table.insert(procs, proc) end
+	print("querying for spawned-on application...")
+	alua.query(app, query3_callback)
+end
+
+function start_callback(reply)
+	print("querying for just-started application...")
+	alua.query(app, query2_callback)
 end
 
 alua.open()
-alua.query("my application", query_callback)
+print("querying for non-existing application...")
+alua.query(app, query1_callback)
 alua.loop()
