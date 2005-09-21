08d6973da9f25146583fc0c9859ccbca866dc2d0
pedro <pedro@inf.puc-rio.br>
2005-09-21 16:05:36 +0000

    new example for alua.spawn()

--- samples/spawn.lua
+++ samples/spawn.lua
@@ -1,20 +1,32 @@
+-- public domain
 alua = require("alua")
 
-function
-spawn_callback(reply)
-  	print("Spawn on " .. reply.name)
-	for id, proc in reply.processes do
-		if proc.status ~= "ok" then
-			print("Failed to spawn " .. id .. ": " .. proc.error)
-		else
-			print(id .. " successfully spawned on " .. proc.daemon)
-		end
-	end
+local procs, app = {}, "my app"
+
+function spawn3_callback(reply)
+	print("reply table is: " .. _alua.utils.dump(reply))
+	alua.exit(procs); alua.exit()
+end
+
+function spawn2_callback(reply)
+	print("reply table is: " .. _alua.utils.dump(reply))
+	print("spawning processes A, B, C and D *again*...")
+	alua.spawn(app, { "A", "B", "C", "D" }, spawn3_callback)
+	for proc in reply.processes do table.insert(procs, proc) end
+end
+
+function spawn1_callback(reply)
+	print("reply table is: " .. _alua.utils.dump(reply))
+	print("spawning processes A, B, C and D...")
+	alua.spawn(app, { "A", "B", "C", "D" }, spawn2_callback)
+	for proc in reply.processes do table.insert(procs, proc) end
+end
+
+function start_callback(reply)
+	print("spawning 7 new processes...")
+	alua.spawn(app, 7, spawn1_callback)
 end
 
 alua.open()
-alua.start("new application")
-alua.spawn("new application", 7, spawn_callback)
-alua.spawn("new application", { "A", "B", "C", "D" }, spawn_callback)
-alua.spawn("new application", { "A", "B", "C", "D" }, spawn_callback)
+alua.start(app, start_callback)
 alua.loop()
