4e415f441f853219d4b43f1f7217aa9347e4921c
pedro <pedro@inf.puc-rio.br>
2005-10-05 11:49:06 +0000

    new example for alua.start()

--- samples/start.lua
+++ samples/start.lua
@@ -1,23 +1,20 @@
+-- public domain
 alua = require("alua")
 
-daemons = { "213.41.150.72:6722", "65.94.34.3:3015", "195.56.229.197:1152",
-	    "62.178.75.222:8919", "81.130.6.2:7834", "202.92.229.140:4930" }
+local app = "my app"
+
+function
+start2_callback(reply)
+	print("reply table is: " .. _alua.utils.dump(reply))
+	alua.exit()
+end
 
 function
 start_callback(reply)
-  	if reply.status ~= "ok" then
-		print("Could not start application: " .. reply.error)
-	else
-		for _, id in daemons do
-			if reply.daemons[id].status ~= "ok" then
-				print("Could not start application on " .. id)
-				print("Error: " .. reply.daemons[id].error)
-			end
-		end
-	end
+	print("reply table is: " .. _alua.utils.dump(reply))
+	alua.start(app, start2_callback)
 end
 
 alua.open()
-alua.start("jose's application", daemons, start_callback)
-alua.start("jose's application", daemons, start_callback)
+alua.start(app, start_callback)
 alua.loop()
