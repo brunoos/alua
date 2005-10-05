48b1bb6af24f2d07b1f1891dc1dedf9fb7b27a1e
pedro <pedro@inf.puc-rio.br>
2005-10-05 11:27:51 +0000

    new example for alua.join()

--- samples/join.lua
+++ samples/join.lua
@@ -1,34 +1,14 @@
+-- public domain
 alua = require("alua")
+
+local app = "my app"
 
 function
 join_callback(reply)
-  	if reply.status ~= "ok" then
-		print("Failed to join application " .. reply.name)
-		print("Error: " .. reply.error)
-	else
-		print("Joined " .. reply.name)
-	end
-end
-
-function
-start_callback(reply)
-  	if reply.status ~= "ok" then
-		print("Failed to create application " .. reply.name)
-		print("Error: " .. reply.error)
-	else
-		print("Created " .. reply.name)
-	end
-end
-
-function
-query_callback(reply)
-  	if reply.processes then
-		alua.join(reply.name, join_callback)
-	else
-		alua.start(reply.name, { "209.241.5.12:5265" }, start_callback)
-	end
+	print("reply table is: " .. _alua.utils.dump(reply))
+	alua.exit()
 end
 
 alua.open()
-alua.query("application x", query_callback)
+alua.join(app, join_callback)
 alua.loop()
