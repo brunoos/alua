1aa2c9b506da9d8cb57faca55c8832388757e4a7
brunoos <brunoos@inf.ufg.br>
2007-10-26 13:58:30 +0000

    Change the module defition to avoid lookup for null fields in the environment.

--- src/alua.lua
+++ src/alua.lua
@@ -5,21 +5,27 @@
 -- in the LICENCE file.
 --
 
-module("alua", package.seeall)
+-- Do not use 'package.seeall' because the fields 'alua.id' and 'alua.daemonid'
+-- can be nil. This will trigger a search in the environment and it can break 
+-- ALua. Instead, use a local variable to access the global environment.
 
-require("alua.channel")
-require("alua.timer")
-require("alua.event")
-require("alua.daemon")
-require("alua.process")
+local env = _G
+
+module("alua")
+
+env.require("alua.channel")
+env.require("alua.timer")
+env.require("alua.event")
+env.require("alua.task")
+env.require("alua.daemon")
+env.require("alua.process")
 
 -- Export some functions
-create  = alua.daemon.create
-send    = alua.process.send
-link    = alua.process.link
-close   = alua.process.close
-connect = alua.process.connect
-spawn   = alua.process.spawn
+create = daemon.create
+send   = process.send
+link   = process.link
+close  = process.close
+spawn  = process.spawn
 
 --
 -- This function opens a connection with a daemon.
@@ -27,23 +33,23 @@ spawn   = alua.process.spawn
 --
 function open(cfg, cb)
    -- Connect to the daemon
-   if type(cfg) == "string" then
-      alua.connect(cfg, cb)
+   if env.type(cfg) == "string" then
+      process.connect(cfg, cb)
    -- Create the daemon before to connect
    elseif type(cfg) == "table" then
       local reply = function(msg)
          if msg.status == "ok" then
-            alua.connect(msg.daemon, cb)
+            process.connect(msg.daemon, cb)
          else
             if cb then
                cb(msg)
             end
          end
       end
-      alua.create(cfg, reply)
+      create(cfg, reply)
    else
       if cb then
-         alua.task.schedule(cb, {status = "error", 
+         task.schedule(cb, {status = "error", 
              error = "invalid #1 argument"})
       end
    end
@@ -54,7 +60,7 @@ end
 --
 function loop()
    while true do
-      alua.timer.poll()
-      alua.channel.poll()
+      timer.poll()
+      channel.poll()
    end
 end
